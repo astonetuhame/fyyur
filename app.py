6548cbb2cfbd676a3ca1a31da2900fb9c91e2c8a
astonetuhame <astonetuhame@gmail.com>
2022-08-11 16:00:08 +0300

    Search artists

--- app.py
+++ app.py
@@ -268,24 +268,30 @@ def delete_venue(venue_id):
 #  Show artists
 @app.route('/artists')
 def artists():
-  artists = Artist.query.with_entities(Artist.id, Artist.name).all()
-  return render_template('pages/artists.html', artists=artists)
+    artists = Artist.query.with_entities(Artist.id, Artist.name).all()
+    return render_template('pages/artists.html', artists=artists)
+
+# Search Artists
 
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term', '')
+    artists = Artist.query.filter(Artist.name.ilike('%'+search_term+'%')).all()
+    data = []
+    artist_count = len(artists)
+    for artist in artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": artist.num_upcoming_shows
+        })
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": artist_count,
+        "data": data
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
